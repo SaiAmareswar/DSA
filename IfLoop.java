9eed19955d39e1f27cc81d58e34df9daeb232649
G Sai Amareswar <91476781+SaiAmareswar@users.noreply.github.com>
2025-08-04 16:19:17 +0000

    if loop for adult or not

---
+++ IfLoop.java
@@ -0,0 +1,12 @@
+public class IfLoop {
+    public static void main(String[] args) {
+        int age=10;
+       
+
+        if (age >= 18) {
+            System.out.println("You are an adult.");
+        } else {
+            System.out.println("You are not an adult.");
+        }
+    }
+}
